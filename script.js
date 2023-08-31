4eaa6c649d0927d1116e187f9d4a4cf7a848d6fa
Merk0n <127611734+Merk0n@users.noreply.github.com>
2023-08-31 21:17:36 +0200

    added calculate function

---
+++ script.js
@@ -0,0 +1,15 @@
+const calculate = (n1, operator, n2) => {
+  let result = "";
+
+  if (operator === "add") {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === "subtract") {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === "multiply") {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === "divide") {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
+
+  return result;
+};
