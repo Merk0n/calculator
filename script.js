e7d8e8292625581ac8da5c75dd6ec6b79dedb18a
Merk0n <127611734+Merk0n@users.noreply.github.com>
2023-09-02 11:43:10 +0200

    working calculator

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-const calculate = (n1, operator, n2) => {
-  let result = "";
+// Get elements from the DOM
+const display = document.querySelector(".display");
+const clearButton = document.getElementById("clear");
+const equalsButton = document.getElementById("equals");
 
-  if (operator === "add") {
-    result = parseFloat(n1) + parseFloat(n2);
-  } else if (operator === "subtract") {
-    result = parseFloat(n1) - parseFloat(n2);
-  } else if (operator === "multiply") {
-    result = parseFloat(n1) * parseFloat(n2);
-  } else if (operator === "divide") {
-    result = parseFloat(n1) / parseFloat(n2);
-  }
+// Add event listeners to buttons
+document.querySelectorAll(".btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.value;
 
-  return result;
-};
+    if (value === "=") {
+      // Evaluate the expression and display the result
+      try {
+        display.value = eval(display.value);
+      } catch (error) {
+        display.value = "Error";
+      }
+    } else if (value === "AC") {
+      // Clear the display
+      display.value = "0";
+    } else if (value === "+-") {
+      // Toggle the sign of the current value
+      display.value = parseFloat(display.value) * -1;
+    } else {
+      // Append the clicked button's value to the display
+      if (display.value === "0" || display.value === "Error") {
+        display.value = value;
+      } else {
+        display.value += value;
+      }
+    }
+  });
+});
